17d03f89a91959b473ca530babe7fa1399fedd74
LongPhamHai <dotrucbk@gmail.com>
2026-03-14 18:20:49 +0700

    add

--- example/Makefile.mk
+++ example/Makefile.mk
@@ -1,27 +1,23 @@
 -include Config.mk
 
 NAME_MODULE = $(EXAMPLE_DIR)/example
-# NAME_STATIC_MODULE = libak.a
+NAME_STATIC_MODULE = libak.a
 
 OBJ += 	$(EXAMPLE_DIR)/main.o \
 		$(EXAMPLE_DIR)/task_list.o \
 		$(EXAMPLE_DIR)/task_1.o \
 		$(EXAMPLE_DIR)/task_2.o 
 
-CFLAGS += -I$(PREFIX)/include/
-LDFLAGS += -L$(PREFIX)/lib
-LDLIBS += -lak
-
 all: $(NAME_MODULE)
 
 $(EXAMPLE_DIR)/%.o: %.c
 	@echo CC 	$<
-	@$(CC) $(CFLAGS) -c -o $@ $<
+	@$(CC) -c -o $@ $< $(CFLAGS) $(LDFLAGS)
 
 $(NAME_MODULE): $(OBJ)
 	@echo ---------- START LINK EXAMPLE ----------
 	@echo CC 	$^ -o $@
-	@$(CC) -o $@ $^ $(LDFLAGS) $(LDLIBS)
+	@$(CC) -o $@ $^ $(OBJ_DIR)/$(NAME_STATIC_MODULE) $(CFLAGS) $(LDFLAGS)
 	
 clean:
 	@echo rm -rf $(EXAMPLE_DIR)/$(OBJ)
