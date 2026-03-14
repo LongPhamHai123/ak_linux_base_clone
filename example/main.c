02abdb0c411de03ae9dc3507e9462e7fb0618ee8
LongPhamHai <dotrucbk@gmail.com>
2026-03-14 20:43:21 +0700

    a

--- example/main.c
+++ example/main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "raylib.h"
 int main() {
 	ak_init_tasks(AK_TASK_LIST_LEN, task_list_init);
 
@@ -7,6 +7,20 @@ int main() {
 
     ak_stop_all_tasks();
 
+    
+
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    
 	return 0;
 }
 
