eaaf43d07013714e34729bcde22774b7e8138028
LongPhamHai <dotrucbk@gmail.com>
2026-03-15 18:47:37 +0700

    a

--- example/task_3.h
+++ example/task_3.h
@@ -1,10 +1,20 @@
 #ifndef __TASK_3_H__
 #define __TASK_3_H__
 
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
 #include "ak/ak.h"
 #include "task_list.h"
-
 extern void* gw_task_app3_entry(void);
 extern q_msg_t gw_task_app3_mailbox;
+#ifdef __cplusplus
+}
+#endif
 
-#endif //__TASK_2_H__
+
+
+
+#endif //__TASK_3_H__
